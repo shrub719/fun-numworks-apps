4ca5435d79a94b5119dd121cea8bd6669564047b
shrub <shrub719@gmail.com>
2025-05-04 20:32:22 +0100

    replaced constant with EADK color

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,5 @@ extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;  // i don't know what this does
 
 int main(int argc, char * argv[]) {
-    EADK::Display::pushRectUniform(EADK::Screen::Rect, Black);
+    EADK::Display::pushRectUniform(EADK::Screen::Rect, EADK::Color Black(0x000000));
 }
