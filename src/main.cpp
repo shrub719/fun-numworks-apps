3b8af27414029ed9344cf8a05f4c182814214fad
shrub <shrub719@gmail.com>
2025-05-04 20:29:05 +0100

    added some header files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "display.h"
+#include "eadkpp.h"
+
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "3D Viewer";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;  // i don't know what this does
 
