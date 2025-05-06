9832d98f4583930a244451ccec18452aab6dac97
shrub <shrub719@gmail.com>
2025-05-06 19:04:35 +0100

    added readme + extapp storage (<3 nwagyu)

--- src/canvas/main.cpp
+++ src/canvas/main.cpp
@@ -5,5 +5,10 @@ extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 int main(int argc, char * argv[]) {
-    
+    Keyboard::State keyboardState = Keyboard::scan();
+
+    Display::pushRectUniform(Screen::Rect, Color(0xFFFFFF));
+    while (!keyboardState.keyDown(Keyboard::Key::Back)) {
+
+    }
 }
