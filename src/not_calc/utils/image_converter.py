01d9285ec9ab5590ce55defc0b07b4a04621e2a4
shrub <shrub719@gmail.com>
2025-05-18 11:50:47 +0100

    fixed extractor script

--- src/not_calc/utils/image_converter.py
+++ src/not_calc/utils/image_converter.py
@@ -6,8 +6,8 @@ pixels = image.load()
 width, height = image.size
 
 print("{\n    ", end="")
-for x in range(width):
-    for y in range(height):
+for y in range(height):
+    for x in range(width):
         r, g, b, p = image.getpixel((x, y))
         h = hex(r*65536 + g*256 + b*1)
         print(f"Color({h}), ", end="")
