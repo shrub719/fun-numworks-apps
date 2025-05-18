96febfd19303202c6ddfd35000f32618f898dc27
shrub <shrub719@gmail.com>
2025-05-18 11:47:52 +0100

    added image extraction python script

---
+++ src/not_calc/utils/image_converter.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+image = Image.open("hat.png")
+pixels = image.load()
+
+width, height = image.size
+
+print("{\n    ", end="")
+for x in range(width):
+    for y in range(height):
+        r, g, b, p = image.getpixel((x, y))
+        h = hex(r*65536 + g*256 + b*1)
+        print(f"Color({h}), ", end="")
+    print("\n    ", end="")
+print("\n}")
