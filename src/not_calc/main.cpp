c749fdea40fde1a5e8eeba3b843513e951efc761
shrub <shrub719@gmail.com>
2025-05-18 11:47:08 +0100

    added structure of not_calc

---
+++ src/not_calc/main.cpp
@@ -0,0 +1,18 @@
+#include "eadkpp.h"
+#include "hat.h"
+using namespace EADK;
+
+extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Calculation";
+extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
+
+const Color ORANGE = Color(0xFFB531);
+
+void draw() {
+    Display::drawString("UK", Point(72, 3), false, Color(0xFFFFFF), ORANGE);
+    Display::pushRect(Rect(93, 5, 18, 9), IMAGE);
+}
+
+int main(int argc, char * argv[]) {
+    draw();
+    while (1) {}
+}
