c60af1fd278e70c57a3d7e128ac4fe9b0366f540
shrub <shrub719@gmail.com>
2025-05-18 20:15:33 +0100

    added pull_rect

--- src/canvas/main.cpp
+++ src/canvas/main.cpp
@@ -1,4 +1,5 @@
 #include "eadkpp.h"
+#include "save-load.h"
 using namespace EADK;
 
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Canvas";
@@ -8,7 +9,13 @@ int main(int argc, char * argv[]) {
     Keyboard::State keyboardState = Keyboard::scan();
 
     Display::pushRectUniform(Screen::Rect, Color(0xFFFFFF));
+    
     while (!keyboardState.keyDown(Keyboard::Key::Back)) {
-
+        keyboardState = Keyboard::scan();
+    }
+    save_canvas(1);     // crashes calculator sometimes
+                        // (why sometimes? don't tell me garbage collection is still an issue)
+    while (!keyboardState.keyDown(Keyboard::Key::One)) {
+        keyboardState = Keyboard::scan();
     }
 }
