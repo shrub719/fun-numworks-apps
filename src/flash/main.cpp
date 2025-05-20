e57a71585e80850f8cf5b8c820fab03d35372dc8
shrub <shrub719@gmail.com>
2025-05-20 12:16:27 +0100

    updated svc stuff

--- src/flash/main.cpp
+++ src/flash/main.cpp
@@ -1,24 +1,26 @@
 #include "eadkpp.h"
 using namespace EADK;
 
+#define SVC_ATTRIBUTES __attribute__((noinline, externally_visible))
+#define SVC_RETURNING_VOID(code) \
+  asm volatile("svc %[immediate]" : : [immediate] "I"(code));
+
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Flashlight";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-void SVC_RETURNING_VOID(int code) {
-    asm volatile("svc %[immediate]" : : [immediate] "I"(code));
+void SVC_ATTRIBUTES setColor(Color c) {
+    asm volatile("mov r0, %0" : : "r"(c));
+    SVC_RETURNING_VOID(37)
 }
 
-void __attribute__((noinline, externally_visible)) setColor(Color c) {
-    SVC_RETURNING_VOID(37);
+void SVC_ATTRIBUTES setLock(bool locked) {
+    asm volatile("mov r0, %0" : : "r"(locked));
+    SVC_RETURNING_VOID(58)
 }
 
-void __attribute__((noinline, externally_visible)) setLock(bool locked) {
-    SVC_RETURNING_VOID(58);
-}
-
-void __attribute__((noinline, externally_visible)) setBlinking(uint16_t periodInMilliseconds, float dutyCycle) {
-    SVC_RETURNING_VOID(36);
-}
+/* void SVC_ATTRIBUTES setBlinking(uint16_t periodInMilliseconds, float dutyCycle) {
+    SVC_RETURNING_VOID(36)
+} */
 
 
 int main(int argc, char * argv[]) {
@@ -27,7 +29,7 @@ int main(int argc, char * argv[]) {
 
     setLock(false);
     setColor(white);
-    setBlinking(1000, 0.1f);
+    // setBlinking(1000, 0.1f);
 
     while (!keyboardState.keyDown(Keyboard::Key::Back)) {
         keyboardState = Keyboard::scan();
